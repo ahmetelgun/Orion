cdf43ad3d0ea88496546932d1e264c0824e1f2c0
Ahmet Elgun <ahmetelgn@gmail.com>
2020-09-30 00:53:09 +0300

    login, logout, postlist and get post test cases added

--- tests.py
+++ tests.py
@@ -1,106 +1,172 @@
 import unittest
 import jwt
-from app import app
 import datetime
 from config import SECRET_KEY
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Author, Tag
 from auth import create_token, set_token_to_user, is_login, register_user
-
+from flask import Flask
+import app
+import json
+from models import Post
+import datetime
 
 def create_db():
-  global engine, session, user, Session
-  engine = create_engine("sqlite:///:memory:")
-  Session = sessionmaker(bind=engine)
-  session = Session()
-  Base.metadata.create_all(engine)
-  user = Author(name="a", username="a", password="12345678")
-  tag1 = Tag(name="tag1")
-  tag2 = Tag(name="tag2")
-  session.add_all([user, tag1, tag2])
-  session.commit()
+    global session
+    global user
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+    session = app.create_session(engine)
+    user = register_user(session, name="ahmet", username="a", password="12345678")
+    tag1 = Tag(name="tag1")
+    tag2 = Tag(name="tag2")
+    post1 = Post(name="post1", published_date=datetime.datetime(2020, 5, 7, 15, 42, 0,0), text="post1 text", excerpt="post1 excerpt")
+    post2 = Post(name="post2", published_date=datetime.datetime(2020, 6, 1, 12, 16, 0,0), text="post2 text", excerpt="post2 excerpt")
+    post3 = Post(name="post3", published_date=datetime.datetime(2020, 7, 25, 5, 23, 0,0), text="post3 text", excerpt="post3 excerpt")
+    post1.tags.append(tag1)
+    post1.tags.append(tag2)
+    post2.tags.append(tag2)
+    post1.author = user
+    post2.author = user
+    post3.author = user
+    session.add_all([tag1, tag2, post1, post2, post3])
+    session.commit()
 
+
+class AuthTestCase(unittest.TestCase):
+    def test_login_endpoint(self):
+        success = eval('{"login": True}')
+        fail = eval('{"login": False, "message": "invalid username or password"}')
+        
+        c = app.app.test_client()
+        res = c.post("/login", json={"username": "a", "password": "12345678"})
+        self.assertEqual(res.json, success)
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", "asdljnsadlksad")
+        res = c.post("/login", json={"username": "a", "password": "12345678"})
+        self.assertEqual(res.json, success)
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/login")
+        self.assertEqual(res.json, success)
+
+        c = app.app.test_client()
+        res = c.post("/login", json={"username": "asd", "password": "12345678"})
+        self.assertEqual(res.json, fail)
+        c = app.app.test_client()
+        res = c.post("/login", json={"username": "", "password": "12345678"})
+        self.assertEqual(res.json, fail)
+        c = app.app.test_client()
+        res = c.post("/login", json={"password": "12345678"})
+        self.assertEqual(res.json, fail)
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", "asdljnsadlksad")
+        res = c.post("/login")
+        self.assertEqual(res.json, fail)
+        c = app.app.test_client()
+        res = c.post("/login")
+        self.assertEqual(res.json, fail)
+    
+    def test_logout_endpoint(self):
+        c = app.app.test_client()
+        c.post("/login", json={"username": "a", "password": "12345678"})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/logout")
+        self.assertEqual(res.json["logout"], True)
+        c = app.app.test_client()
+        c.post("/login", json={"username": "a", "password": "12345678"})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", "test")
+        res = c.post("/logout")
+        self.assertEqual(res.json["logout"], False)
+
+class PostsEndpointTestCase(unittest.TestCase):
+    def test_posts_endpoint(self):
+        app.posts_per_page = 2
+        c = app.app.test_client()
+        res = c.get("/posts").json
+        self.assertEqual(res['current_page'], 1)
+        self.assertEqual(res['total_number_of_page'], 2)
+        self.assertEqual(res['posts'][0], {'author': 'ahmet', 'author_id': 1, 'excerpt': 'post3 excerpt', 'id': 3, 'name': 'post3', 'published_date': '2020-07-25-05-23', 'tags': []})
+        self.assertEqual(res['posts'][1], {'author': 'ahmet', 'author_id': 1, 'excerpt': 'post2 excerpt', 'id': 2, 'name': 'post2', 'published_date': '2020-06-01-12-16', 'tags': [{'id': 2, 'name': 'tag2'}]})
+        c = app.app.test_client()
+        res = c.get("/posts?page=2").json
+        self.assertEqual(res['current_page'], 2)
+        self.assertEqual(res['total_number_of_page'], 2)
+        self.assertEqual(res['posts'][0], {'author': 'ahmet', 'author_id': 1, 'excerpt': 'post1 excerpt', 'id': 1, 'name': 'post1', 'published_date': '2020-05-07-15-42', 'tags': [{'id': 1, 'name': 'tag1'}, {'id': 2, 'name': 'tag2'}]})
+        c = app.app.test_client()
+        res = c.get("/posts?tag=tag2").json
+        self.assertEqual(res['current_page'], 1)
+        self.assertEqual(res['total_number_of_page'], 1)
+        self.assertEqual(res['posts'][0], {'author': 'ahmet', 'author_id': 1, 'excerpt': 'post2 excerpt', 'id': 2, 'name': 'post2', 'published_date': '2020-06-01-12-16', 'tags': [{'id': 2, 'name': 'tag2'}]})
+        self.assertEqual(res['posts'][1], {'author': 'ahmet', 'author_id': 1, 'excerpt': 'post1 excerpt', 'id': 1, 'name': 'post1', 'published_date': '2020-05-07-15-42', 'tags': [{'id': 1, 'name': 'tag1'}, {'id': 2, 'name': 'tag2'}]})
+        c = app.app.test_client()
+        res = c.get("/posts?tag=notexisttag").json
+        self.assertEqual(res, {'message': 'tag not found'})
+        c = app.app.test_client()
+        res = c.get("/posts?page=notexistpage").json
+        self.assertEqual(res['current_page'], 1)
+        self.assertEqual(res['total_number_of_page'], 2)
+
+class GetPostEndpointTestCase(unittest.TestCase):
+    def test_get_post_endpoint(self):
+        c = app.app.test_client()
+        res = c.get("/2020/07/25/post3").json 
+        self.assertEqual(res, {'author_id': 1, 'id': 3, 'name': 'post3', 'published_date': 'Sat, 25 Jul 2020 05:23:00 GMT', 'tags': [], 'text': 'post3 text'})
+        c = app.app.test_client()
+        res = c.get("/2020/06/01/post2").json 
+        self.assertEqual(res, {'author_id': 1, 'id': 2, 'name': 'post2', 'published_date': 'Mon, 01 Jun 2020 12:16:00 GMT', 'tags': [{'id': 2, 'name': 'tag2'}], 'text': 'post2 text'})
 
 class SetTokenToUserTestCase(unittest.TestCase):
-  def test_set_token_to_user(self):
-    token = "token"
-    set_token_to_user(user, token, session)
-    self.assertEqual(user.token, token)
-
+    def test_set_token_to_user(self):
+        token = "token"
+        set_token_to_user(user, token, session)
+        self.assertEqual(user.token, token)
+        
 
 class CreateTokenTestCase(unittest.TestCase):
-  def test_create_token(self):
-    time = datetime.datetime(5, 5, 1)
-    token = jwt.encode(
-        {"username": "a", "exp": time.timestamp()}, SECRET_KEY, algorithm="HS256").decode("utf-8")
-    self.assertEqual(create_token("a", time), token)
-
-
-class IsLoginTestCase(unittest.TestCase):
-  def test_is_login(self):
-    expiration_time = datetime.datetime.now() + datetime.timedelta(days=1)
-    token = create_token(user.username, expiration_time)
-    expired_token = create_token(
-        user.username, datetime.datetime.now())
-    false_token = create_token("b", expiration_time)
-
-    set_token_to_user(user, token, session)
-    request1 = app.test_request_context(
-        '/login', method='POST', json={"username": "a", "password": "qwer1234"})
-    request2 = app.test_request_context(
-        '/login', method='POST', json={"token": token})
-    request3 = app.test_request_context(
-        '/login', method='POST', json={"username": "a", "password": "qwer1234", "token": token})
-    request4 = app.test_request_context(
-        '/login', method='POST', json={"username": "a", "password": "qwer12345", "token": token})
-    request5 = app.test_request_context(
-        '/login', method='POST', json={"username": "a", "password": "qwer1234", "token": expired_token})
-    request6 = app.test_request_context(
-        '/login', method='POST', json={"username": "a", "password": "qwer1234", "token": false_token})
-    self.assertEqual(is_login(request1.request, session), False)
-    self.assertEqual(is_login(request2.request, session), user)
-    self.assertEqual(is_login(request3.request, session), user)
-    self.assertEqual(is_login(request4.request, session), user)
-    self.assertEqual(is_login(request5.request, session), False)
-    self.assertEqual(is_login(request6.request, session), False)
+    def test_create_token(self):
+        time = datetime.datetime(5, 5, 1)
+        token = jwt.encode(
+            {"username": "a", "exp": time.timestamp()}, SECRET_KEY, algorithm="HS256").decode("utf-8")
+        self.assertEqual(create_token("a", time), token)
 
 
 class RegisterUserTestCase(unittest.TestCase):
-  def test_register_user(self):
-    user1 = register_user(
-        session=session, name="a",
-        username=user.username, password="12345678", )
-    user2 = register_user(
-        session=session, name="qwe",
-        username="abc", password="123"
-    )
-    user3 = register_user(
-        session=session, name="a",
-        username="abd", password="123456789012345678901234567890123"
-    )
-    user4 = register_user(
-        session=session, name="",
-        username="abe", password="12345678"
-    )
-    user5 = register_user(
-        session=session, name="qwe",
-        username="", password="12345678"
-    )
-    user6 = register_user(
-        session=session, name="qwe",
-        username="abe", password="12345678"
-    )
-    self.assertFalse(user1)
-    self.assertFalse(user2)
-    self.assertFalse(user3)
-    self.assertFalse(user4)
-    self.assertFalse(user5)
-    self.assertEqual(user6.username, "abe")
+    def test_register_user(self):
+        user1 = register_user(
+            session=session, name="a",
+            username=user.username, password="12345678", )
+        user2 = register_user(
+            session=session, name="qwe",
+            username="abc", password="123"
+        )
+        user3 = register_user(
+            session=session, name="a",
+            username="abd", password="123456789012345678901234567890123"
+        )
+        user4 = register_user(
+            session=session, name="",
+            username="abe", password="12345678"
+        )
+        user5 = register_user(
+            session=session, name="qwe",
+            username="", password="12345678"
+        )
+        user6 = register_user(
+            session=session, name="qwe",
+            username="abe", password="12345678"
+        )
+        self.assertFalse(user1)
+        self.assertFalse(user2)
+        self.assertFalse(user3)
+        self.assertFalse(user4)
+        self.assertFalse(user5)
+        self.assertEqual(user6.username, "abe")
 
 
 if __name__ == '__main__':
-  create_db()
-  unittest.main()
-  engine.dispose()
-  session.close()
+    create_db()
+    unittest.main()
