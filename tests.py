4703458351f73b6e447a0eaca908306e8a2aeb7f
Ahmet Elgun <ahmetelgn@gmail.com>
2020-10-01 00:30:02 +0300

    taglist, createpost and createtag endpoints tests added

--- tests.py
+++ tests.py
@@ -33,9 +33,9 @@ def create_db():
     session.add_all([tag1, tag2, post1, post2, post3])
     session.commit()
 
-
 class AuthTestCase(unittest.TestCase):
     def test_login_endpoint(self):
+        create_db()
         success = eval('{"login": True}')
         fail = eval('{"login": False, "message": "invalid username or password"}')
         
@@ -69,6 +69,7 @@ class AuthTestCase(unittest.TestCase):
         self.assertEqual(res.json, fail)
     
     def test_logout_endpoint(self):
+        create_db()
         c = app.app.test_client()
         c.post("/login", json={"username": "a", "password": "12345678"})
         c = app.app.test_client()
@@ -84,6 +85,7 @@ class AuthTestCase(unittest.TestCase):
 
 class PostsEndpointTestCase(unittest.TestCase):
     def test_posts_endpoint(self):
+        create_db()
         app.posts_per_page = 2
         c = app.app.test_client()
         res = c.get("/posts").json
@@ -112,6 +114,7 @@ class PostsEndpointTestCase(unittest.TestCase):
 
 class GetPostEndpointTestCase(unittest.TestCase):
     def test_get_post_endpoint(self):
+        create_db()
         c = app.app.test_client()
         res = c.get("/2020/07/25/post3").json 
         self.assertEqual(res, {'author_id': 1, 'id': 3, 'name': 'post3', 'published_date': 'Sat, 25 Jul 2020 05:23:00 GMT', 'tags': [], 'text': 'post3 text'})
@@ -119,23 +122,81 @@ class GetPostEndpointTestCase(unittest.TestCase):
         res = c.get("/2020/06/01/post2").json 
         self.assertEqual(res, {'author_id': 1, 'id': 2, 'name': 'post2', 'published_date': 'Mon, 01 Jun 2020 12:16:00 GMT', 'tags': [{'id': 2, 'name': 'tag2'}], 'text': 'post2 text'})
 
+class TagListEndpointTestCase(unittest.TestCase):
+    def test_tag_list_endpoint(self):
+        create_db()
+        c = app.app.test_client()
+        res = c.get("/taglist").json
+        self.assertEqual(res, [{'id': 1, 'name': 'tag1'}, {'id': 2, 'name': 'tag2'}])
+
+class CreatePostEndpointTestCase(unittest.TestCase):
+    def test_create_post_endpoint(self):
+        create_db()
+        c = app.app.test_client()
+        res = c.post("/login", json={"username": "a", "password": "12345678"})
+        
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", "asdljnsadlksad")
+        res = c.post("/createpost", json={"post_name": "post_name", "post_text": "post_text"}).json
+        self.assertEqual(res, {'login': False, 'message': 'unauthorized request'})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createpost", json={"post_name": "post_name", "post_text": "post_text", "tags": ["tag1", "tag2"]}).json
+        self.assertEqual(res, {'message': 'post added'})
+        self.assertEqual(session.query(Post).all()[-1].name, 'post_name')
+        self.assertEqual(session.query(Post).all()[-1].text, 'post_text')
+        self.assertEqual(session.query(Post).all()[-1].tags[0].name, 'tag1')
+        self.assertEqual(session.query(Post).all()[-1].tags[1].name, 'tag2')
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createpost", json={ "post_text": "post_text", "tags": ["tag1", "tag2"]}).json
+        self.assertEqual(res, {'message': 'post name or post body is invalid'})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createpost", json={ "post_name": "post_name", "tags": ["tag1", "tag2"]}).json
+        self.assertEqual(res, {'message': 'post name or post body is invalid'})
+
+class CreateTagEndpointTestCase(unittest.TestCase):
+    def test_create_tag_endpoint(self):
+        create_db()
+        c = app.app.test_client()
+        res = c.post("/login", json={"username": "a", "password": "12345678"})
+
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", "asdljnsadlksad")
+        res = c.post("/createtag", json={"tag_name": "tag3"}).json
+        self.assertEqual(res, {'login': False, 'message': 'unauthorized request'})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createtag").json
+        self.assertEqual(res, {"message": "tag name is invalid"})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createtag", json={"tag_name": "tag2"}).json
+        self.assertEqual(res, {"message": "tag is exist"})
+        c = app.app.test_client()
+        c.set_cookie("localhost", "token", user.token)
+        res = c.post("/createtag", json={"tag_name": "tag3"}).json
+        self.assertEqual(res, {"message": "tag is added"})
+
 class SetTokenToUserTestCase(unittest.TestCase):
     def test_set_token_to_user(self):
+        create_db()
         token = "token"
         set_token_to_user(user, token, session)
         self.assertEqual(user.token, token)
         
-
 class CreateTokenTestCase(unittest.TestCase):
     def test_create_token(self):
+        create_db()
         time = datetime.datetime(5, 5, 1)
         token = jwt.encode(
             {"username": "a", "exp": time.timestamp()}, SECRET_KEY, algorithm="HS256").decode("utf-8")
         self.assertEqual(create_token("a", time), token)
 
-
 class RegisterUserTestCase(unittest.TestCase):
     def test_register_user(self):
+        create_db()
         user1 = register_user(
             session=session, name="a",
             username=user.username, password="12345678", )
@@ -166,7 +227,5 @@ class RegisterUserTestCase(unittest.TestCase):
         self.assertFalse(user5)
         self.assertEqual(user6.username, "abe")
 
-
 if __name__ == '__main__':
-    create_db()
     unittest.main()
