dc4df5ee9efb31caa233497feb317d44c7000217
Ahmet Elgun <ahmetelgn@gmail.com>
2020-12-18 11:44:53 +0300

    isLogin cookie added

--- app.py
+++ app.py
@@ -12,9 +12,19 @@ from sqlalchemy import func
 from models import create_session
 app = Flask(__name__)
 
+engine = create_engine(database_url)
+session = create_session(engine)
+app.debug = True
+
 
 def make_response_with_token(body, token):
     resp = jsonify(body)
+    if token == "":
+        is_login = "false"
+    else:
+        is_login = "true"
+
+    resp.set_cookie("isLogin", is_login, samesite='Strict')
     resp.set_cookie("token", token, secure=True,
                     httponly=True, samesite='Strict')
     return resp
@@ -30,17 +40,16 @@ def login():
         content = request.json
         username = html.escape(content["username"])
         password = content["password"]
-    except:  # if username or password not exist in request
-        return jsonify({"login": False, "message": "invalid username or password"}), 401
+    except:
+        return make_response_with_token({"login": False, "message": "invalid username or password"}, ""), 401
     user = session.query(Author).filter_by(username=username).first()
     if user:
         if check_password_hash(user.password, password):
             expiration_time = datetime.datetime.now() + datetime.timedelta(days=1)
             token = create_token(user.username, expiration_time)
             set_token_to_user(user, token, session)
-            resp = make_response_with_token({"login": True}, token)
-            return resp, 200
-    return jsonify({"login": False, "message": "invalid username or password"}), 401
+            return make_response_with_token({"login": True}, token), 200
+    return make_response_with_token({"login": False, "message": "invalid username or password"}, ""), 401
 
 
 @app.route('/logout', methods=["POST"])
@@ -50,13 +59,12 @@ def logout():
         user.token = ""
         session.add(user)
         session.commit()
-        res = make_response_with_token({"logout": True}, "")
-        return res, 200
-    return jsonify({"logout": False}), 400
+    return make_response_with_token({"logout": True}, ""), 200
 
 
 @app.route('/posts', methods=["GET"])
 def posts():
+    user = is_login(request, session)
     total_number_of_page = 1
     postslist = []
     tag = request.args.get('tag', default=None, type=str)
@@ -67,7 +75,9 @@ def posts():
                 .filter(Post.tags.contains(tag))\
                 .group_by(Post).order_by(Post.published_date.desc()).all()
         else:
-            return jsonify({"message": "tag not found"}), 404
+            if user:
+                return make_response_with_token({"message": "tag not found"}, user.token), 404
+            return make_response_with_token({"message": "tag not found"}, ""), 404
     else:  # if not tag query in query string
         posts = session.query(Post).order_by(Post.published_date.desc()).all()
     if len(posts) > posts_per_page:
@@ -91,11 +101,16 @@ def posts():
             .strftime('%d.%m.%Y')
         post.pop('text')
         postslist.append(post)
-    return jsonify({"posts": postslist, 'current_page': current_page, 'total_number_of_page': total_number_of_page})
+    body = {"posts": postslist, 'current_page': current_page,
+            'total_number_of_page': total_number_of_page}
+    if user:
+        return make_response_with_token(body, user.token), 200
+    return make_response_with_token(body, ""), 200
 
 
 @app.route('/<int:year>/<int:month>/<int:day>/<string:name>')
 def post(year, month, day, name):
+    user = is_login(request, session)
     endpoint = f"/{year}/{month}/{day}/{name}"
     post = session.query(Post).filter_by(endpoint=endpoint).first()
     if post:
@@ -108,32 +123,39 @@ def post(year, month, day, name):
             .strftime('%d.%m.%Y')
         res.pop('_sa_instance_state', None)
         res.pop('excerpt')
-        return jsonify(res), 200
-    return jsonify({"message": "post not found"}), 404
+        if user:
+            return make_response_with_token(res, user.token), 200
+        return make_response_with_token(res, ""), 200
+    if user:
+        return make_response_with_token({"message": "post not found"}, user.token), 404
+    return make_response_with_token({"message": "post not found"}, ""), 404
 
 
 @app.route('/taglist')
 def taglist():
+    user = is_login(request, session)
     tags_all = session.query(Tag).all()
     tags = []
     for tag in tags_all:
         t = tag.__dict__
         t.pop('_sa_instance_state', None)
         tags.append(t)
-    return jsonify(tags), 200
+    if user:
+        return make_response_with_token(tags, user.token), 200
+    return make_response_with_token(tags, ""), 200
 
 
 @app.route('/createpost', methods=["POST"])
 def addpost():
     user = is_login(request, session)
     if not user:
-        return jsonify({"login": False, "message": "unauthorized request"}), 401
+        return make_response_with_token({"login": False, "message": "unauthorized request"}, ""), 401
     try:
         content = request.json
         post_name = html.escape(content["post_name"])
         post_text = content["post_text"]
     except:
-        return jsonify({"message": "post name or post body invalid"}), 400
+        return make_response_with_token({"message": "post name or post body invalid"}, user.token), 400
     published_date = datetime.datetime.now()
     endpoint = f"/{published_date.year}/{published_date.month}/{published_date.day}/{'-'.join(post_name.split()).lower()}"
     temp_endpoint = endpoint
@@ -154,25 +176,25 @@ def addpost():
     post.author = user
     session.add(post)
     session.commit()
-    return jsonify({"message": "post added"}), 200
+    return make_response_with_token({"message": "post added"}, user.token), 200
 
 
 @app.route('/editpost', methods=["POST"])
 def editpost():
     user = is_login(request, session)
     if not user:
-        return jsonify({"login": False, "message": "unauthorized request"}), 401
+        return make_response_with_token({"login": False, "message": "unauthorized request"}, ""), 401
     try:
         content = request.json
         post_name = html.escape(content["name"])
         post_text = content["text"]
         post_id = content["id"]
     except:
-        return jsonify({"message": "post name or post body invalid"}), 400
+        return make_response_with_token({"message": "post name or post body invalid"}, user.token), 400
     post = session.query(Post).filter_by(id=post_id).first()
     if post:
         if post.author.id != user.id:
-            return jsonify({"login": False, "message": "unauthorized request"}), 401
+            return make_response_with_token({"message": "unauthorized request"}, user.token), 401
         post.name = post_name
         post.text = post_text
         try:
@@ -181,64 +203,68 @@ def editpost():
             pass
         session.add(post)
         session.commit()
-        return jsonify({"message": "post updated"}), 200
-    return jsonify({"message": "post not found"}), 404
+        return make_response_with_token({"message": "post updated"}, user.token), 200
+    return make_response_with_token({"message": "post not found"}, user.token), 404
 
 
 @app.route('/createtag', methods=["POST"])
 def addtag():
+    import time
+    time.sleep(3)
     user = is_login(request, session)
     if not user:
-        return jsonify({"login": False, "message": "unauthorized request"}), 401
+        return make_response_with_token({"login": False, "message": "unauthorized request"}, ""), 401
     try:
         content = request.json
         tag_name = content['tag_name']
     except:
-        return jsonify({"message": "tag name invalid"}), 400
+        return make_response_with_token({"message": "tag name invalid"}, user.token), 400
     tag = session.query(Tag).filter(func.lower(Tag.name) == tag_name).first()
     if tag:
-        return jsonify({"message": "tag exist"}), 409
+        return make_response_with_token({"message": "tag exist"}, user.token), 409
     tag = Tag(name=tag_name)
     session.add(tag)
     session.commit()
-    return jsonify({"message": "tag added"}), 200
+    return make_response_with_token({"message": "tag added"}, user.token), 200
 
 # for custom pages like about, contact
 @app.route('/<string:custom>', methods=["GET"])
 def customPages(custom):
+    user = is_login(request, session)
     page = session.query(CustomPage).filter_by(endpoint=f"/{custom}").first()
     if page:
         res = page.__dict__
         res.pop('_sa_instance_state', None)
-        return jsonify(res), 200
-    return jsonify({"message": "page not found"}), 404
+        if user:
+            return make_response_with_token(res, user.token), 200
+        return make_response_with_token(res, ""), 200
+    if user:
+        return make_response_with_token({"message": "page not found"}, user.token), 404
+    return make_response_with_token({"message": "page not found"}, ""), 404
 
 
 @app.route('/createcustompage', methods=["POST"])
 def createCustomPage():
     user = is_login(request, session)
     if not user:
-        return jsonify({"login": False, "message": "unauthorized request"}), 401
+        return make_response_with_token({"login": False, "message": "unauthorized request"}, ""), 401
     try:
         content = request.json
         page_name = html.escape(content["name"])
         page_text = content["text"]
         page_endpoint = content["endpoint"]
     except:
-        return jsonify({"message": "page name, text or endpoint invalid"}), 400
+        return make_response_with_token({"message": "page name, text or endpoint invalid"}, user.token), 400
     page_exist = session.query(CustomPage).filter_by(
         endpoint=page_endpoint).first()
     if page_exist:
-        return jsonify({"message": "page exist"}), 409
+        return make_response_with_token({"message": "page exist"}, user.token), 409
     page = CustomPage(name=page_name, text=page_text, endpoint=page_endpoint)
     session.add(page)
     session.commit()
-    return jsonify({"message": "page added"}), 200
+    return make_response_with_token({"message": "page added"}, user.token), 200
 
 
 if __name__ == '__main__':
-    global session
-    engine = create_engine(database_url)
-    session = create_session(engine)
-    app.debug = True
+
     app.run()
