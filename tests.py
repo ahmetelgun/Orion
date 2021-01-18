d8ece677cd691a7d4c1d8a13de7a8fabd69fccfa
Ahmet Elgun <ahmetelgn@gmail.com>
2021-01-18 04:27:08 +0300

    update test

--- tests.py
+++ tests.py
@@ -1,68 +1,129 @@
-import unittest
-import jwt
-import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, Author, Tag, CustomPage
-from auth import create_token, set_token_to_user, is_login, register_user
+import datetime as dt
+from datetime import datetime as dtdt
 from flask import Flask
-import app
 import json
-from models import Post
-import datetime
+import jwt
+import unittest
+import app
+from auth import create_token, set_token_to_user, is_login, register_user, is_time_expire, refresh_token
+from models import Author, Tag, CustomPage, Post
+from test_helpers import SECRET_KEY, create_db, login_user
 
-SECRET_KEY = "secret"
-
-def create_db():
-    global session
-    global user1
-    global user2
-    engine = create_engine("sqlite:///:memory:")
-    Base.metadata.create_all(engine)
-    session = app.create_session(engine)
-    app.session = session
-    user1 = register_user(session, name="Gandalf",
-                          username="gandalf", password="iloveugaladriel")
-    user2 = register_user(session, name="Theoden",
-                          username="horsemaster", password="fudenethor")
-    tag1 = Tag(name="tag1")
-    tag2 = Tag(name="tag2")
-
-    post1 = Post(name="post1", published_date=datetime.datetime(2019, 5, 7, 15, 42, 0, 0),
-                 text="post1 text", excerpt="post1 excerpt", endpoint="/2019/5/7/post1", id=1)
-    post2 = Post(name="post2", published_date=datetime.datetime(2020, 6, 1, 12, 16, 0, 0),
-                 text="post2 text", excerpt="post2 excerpt", endpoint="/2020/6/1/post2", id=2)
-    post3 = Post(name="post3", published_date=datetime.datetime(2020, 6, 25, 5, 23, 0, 0),
-                 text="post3 text", excerpt="post3 excerpt", endpoint="/2020/6/25/post3", id=3)
-    post4 = Post(name="post4", published_date=datetime.datetime(2020, 7, 25, 5, 23, 0, 0),
-                 text="post4 text", excerpt="post4 excerpt", endpoint="/2020/7/25/post4", id=4)
-    post5 = Post(name="post5", published_date=datetime.datetime(2020, 7, 25, 7, 23, 0, 0),
-                 text="post5 text", excerpt="post5 excerpt", endpoint="/2020/7/25/post5", id=5)
-
-    custom = CustomPage(name="About", endpoint="/about", text="its my about")
-
-    post1.tags.append(tag1)
-    post1.tags.append(tag2)
-    post2.tags.append(tag2)
-    post4.tags.append(tag2)
-    post1.author = user1
-    post2.author = user1
-    post3.author = user1
-    post4.author = user2
-    post5.author = user2
-    session.add_all([tag1, tag2, post1, post2, post3, post4, custom])
-    session.commit()
+"""
+    set_token_to_user
+    create_token
+    is_time_expire
+    refresh_token
+    is_login
+    register_user
+"""
 
 
-def login_user(user, session):
-    expiration_time = datetime.datetime.now() + datetime.timedelta(days=1)
-    token = create_token(user.username, expiration_time, SECRET_KEY)
-    set_token_to_user(user, token, session)
+class AuthPyTestCase(unittest.TestCase):
+    def test_set_token_to_user(self):
+        session, user1, _ = create_db()
+        token = "token"
+        set_token_to_user(session, user1, token)
+        self.assertEqual(user1.token, token)
+
+    def test_create_token(self):
+        time = dtdt(5, 5, 1)
+        token = jwt.encode(
+            {"username": "a", "exp": time.timestamp()}, SECRET_KEY, algorithm="HS256").decode("utf-8")
+        self.assertEqual(create_token("a", time, SECRET_KEY), token)
+
+    def test_is_time_expire(self):
+        t = dtdt.now() - dt.timedelta(seconds=1)
+        self.assertTrue(is_time_expire(t))
+
+        t = dtdt.now() + dt.timedelta(seconds=1)
+        self.assertFalse(is_time_expire(t))
+
+    def test_refresh_token(self):
+        expiry_time = dtdt.now() + dt.timedelta(hours=3)
+        is_refresh = refresh_token("gandalf", expiry_time, 1, 1, "secret")
+        self.assertEqual(is_refresh, False)
+
+        expiry_time = dtdt.now() + dt.timedelta(hours=3)
+        is_refresh = refresh_token("gandalf", expiry_time, 1, 4, "secret")
+        self.assertEqual(type(is_refresh).__name__, "str")
+
+    def test_is_login(self):
+        session, _, _ = create_db()
+        self.assertFalse(is_login(session, "token", "secret"))
+
+        expiry_time = dtdt.now()
+        token = create_token("gandalf", expiry_time, "secret")
+        self.assertFalse(is_login(session, token, "secret"))
+
+        expiry_time = dtdt.now() + dt.timedelta(days=1)
+        token = create_token("balrog", expiry_time, "secret")
+        self.assertFalse(is_login(session, token, "secret"))
+
+        expiry_time1 = dtdt.now() + dt.timedelta(days=1)
+        expiry_time2 = dtdt.now() + dt.timedelta(days=2)
+        token1 = create_token("gandalf", expiry_time1, "secret")
+        token2 = create_token("gandalf", expiry_time2, "secret")
+        gandalf = session.query(Author).filter_by(
+            username="gandalf").first()
+        set_token_to_user(session, gandalf, token1)
+        self.assertFalse(is_login(session, token2, "secret"))
+
+        expiry_time = dtdt.now() + dt.timedelta(minutes=1)
+        token = create_token("gandalf", expiry_time, "secret")
+        gandalf = session.query(Author).filter_by(
+            username="gandalf").first()
+        set_token_to_user(session, gandalf, token)
+        user = is_login(session, token, "secret")
+        self.assertEqual(user.username, gandalf.username)
+        self.assertNotEqual(token, user.token)
+
+        expiry_time = dtdt.now() + dt.timedelta(hours=4)
+        token = create_token("gandalf", expiry_time, "secret")
+        gandalf = session.query(Author).filter_by(
+            username="gandalf").first()
+        set_token_to_user(session, gandalf, token)
+        user = is_login(session, token, "secret")
+        self.assertEqual(user.username, gandalf.username)
+        self.assertEqual(token, user.token)
+
+    def test_register_user(self):
+        session, _, _ = create_db()
+
+        self.assertEqual(register_user(session, "Saruman", "crazyboy"), 0)
+        self.assertEqual(register_user(
+            session, "Saruman", password="gandalfscum"), 0)
+        self.assertEqual(register_user(
+            session, username="crazyboy", password="gandalfscum"), 0)
+        self.assertEqual(register_user(
+            session, "Sa", "crazyboy", "gandalfscum"), 1)
+        self.assertEqual(register_user(
+            session, "SarumanSarumanSarumanSarumanSaruman", "crazyboy", "gandalfscum"), 1)
+        self.assertEqual(register_user(
+            session, "Saruman", "cr", "gandalfscum"), 2)
+        self.assertEqual(register_user(
+            session, "Saruman", "crazyboycrazyboycrazyboycrazyboycrazyboy", "gandalfscum"), 2)
+        self.assertEqual(register_user(
+            session, "Saruman", "crazyboy", "gandalf"), 3)
+        self.assertEqual(register_user(
+            session, "Saruman", "crazyboy", "gandalfscumgandalfscumgandalfscum"), 3)
+        self.assertEqual(register_user(
+            session, "Saruman", "gandalf", "gandalfscum"), 4)
+        self.assertEqual(register_user(
+            "session", "Saruman", "crazyboy", "gandalfscum"), 5)
+        self.assertEqual(register_user(session, "Saruman",
+                                       "crazyboy", "gandalfscum").username, "crazyboy")
 
 
-class AuthTestCase(unittest.TestCase):
+"""
+    /login
+    /logout
+"""
+
+
+class AuthenticationEndpoints(unittest.TestCase):
     def test_login_endpoint(self):
-        create_db()
+        session, _, user2 = create_db()
         c = app.app.test_client()
 
         res = c.post(
@@ -112,7 +173,7 @@ class AuthTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_logout_endpoint(self):
-        create_db()
+        session, user1, _ = create_db()
         c = app.app.test_client()
 
         login_user(user1, session)
@@ -125,7 +186,14 @@ class AuthTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
 
 
-class PostListEndpointTestCase(unittest.TestCase):
+"""
+    /posts
+    /yyyy/mm/dd/name
+    /createpost
+"""
+
+
+class PostEndpointsTestCase(unittest.TestCase):
     def test_postlist_endpoint(self):
         create_db()
         app.posts_per_page = 2
@@ -185,8 +253,6 @@ class PostListEndpointTestCase(unittest.TestCase):
         res = c.get("/posts?tag=notexisttag&page=1")
         self.assertEqual(res.status_code, 404)
 
-
-class GetPostEndpointTestCase(unittest.TestCase):
     def test_get_post_endpoint(self):
         create_db()
         c = app.app.test_client()
@@ -202,19 +268,8 @@ class GetPostEndpointTestCase(unittest.TestCase):
         res = c.get("/2020/06/01/post3")
         self.assertEqual(res.status_code, 404)
 
-
-class TagListEndpointTestCase(unittest.TestCase):
-    def test_tag_list_endpoint(self):
-        create_db()
-        c = app.app.test_client()
-        res = c.get("/taglist")
-        self.assertEqual(
-            res.json, ["tag1", "tag2"])
-
-
-class CreatePostEndpointTestCase(unittest.TestCase):
     def test_create_post_endpoint(self):
-        create_db()
+        session, user1, _ = create_db()
         c = app.app.test_client()
         login_user(user1, session)
 
@@ -256,85 +311,8 @@ class CreatePostEndpointTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(session.query(Post).all()[-1].tags[0].name, 'tag1')
 
-
-class CreateTagEndpointTestCase(unittest.TestCase):
-    def test_create_tag_endpoint(self):
-        create_db()
-        c = app.app.test_client()
-        login_user(user1, session)
-
-        c.set_cookie("localhost", "token", "asdljnsadlksad")
-        res = c.post("/createtag", json={"tag_name": "tag3"})
-        self.assertEqual(res.status_code, 401)
-
-        c.set_cookie("localhost", "token", user1.token)
-        res = c.post("/createtag")
-        self.assertEqual(res.status_code, 400)
-
-        c.set_cookie("localhost", "token", user1.token)
-        res = c.post("/createtag", json={"tag_name": "tag2"})
-        self.assertEqual(res.status_code, 409)
-
-        c.set_cookie("localhost", "token", user1.token)
-        res = c.post("/createtag", json={"tag_name": "tag3"})
-        self.assertEqual(res.status_code, 200)
-
-
-class SetTokenToUserTestCase(unittest.TestCase):
-    def test_set_token_to_user(self):
-        create_db()
-        token = "token"
-        set_token_to_user(user1, token, session)
-        self.assertEqual(user1.token, token)
-
-
-class CreateTokenTestCase(unittest.TestCase):
-    def test_create_token(self):
-        create_db()
-        time = datetime.datetime(5, 5, 1)
-        token = jwt.encode(
-            {"username": "a", "exp": time.timestamp()}, SECRET_KEY, algorithm="HS256").decode("utf-8")
-        self.assertEqual(create_token("a", time, SECRET_KEY), token)
-
-
-class RegisterUserTestCase(unittest.TestCase):
-    def test_register_user(self):
-        create_db()
-
-        test_user1 = register_user(
-            session=session, name="Manwe",
-            username=user1.username, password="12345678", )
-        test_user2 = register_user(
-            session=session, name="Irmo",
-            username="Irmo", password="123"
-        )
-        test_user3 = register_user(
-            session=session, name="Orome",
-            username="orome", password="123456789012345678901234567890123"
-        )
-        test_user4 = register_user(
-            session=session, name="",
-            username="ulmo", password="12345678"
-        )
-        test_user5 = register_user(
-            session=session, name="Aule",
-            username="", password="12345678"
-        )
-        test_user6 = register_user(
-            session=session, name="Melkor",
-            username="melkor", password="12345678"
-        )
-        self.assertFalse(test_user1)
-        self.assertFalse(test_user2)
-        self.assertFalse(test_user3)
-        self.assertFalse(test_user4)
-        self.assertFalse(test_user5)
-        self.assertEqual(test_user6.username, "melkor")
-
-
-class EditPostEndpointTestCase(unittest.TestCase):
     def test_editpost_endpoint(self):
-        create_db()
+        session, user1, user2 = create_db()
         c = app.app.test_client()
         login_user(user1, session)
         login_user(user2, session)
@@ -369,11 +347,53 @@ class EditPostEndpointTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
 
 
-class CreateCustomPageEndpointTestCase(unittest.TestCase):
-    def test_createcustompage_endpoint(self):
+"""
+    /taglist
+    /createtag
+"""
+
+
+class TagEnpointsTestCase(unittest.TestCase):
+    def test_tag_list_endpoint(self):
         create_db()
         c = app.app.test_client()
-        
+        res = c.get("/taglist")
+        self.assertEqual(
+            res.json, ["tag1", "tag2"])
+
+    def test_create_tag_endpoint(self):
+        session, user1, _ = create_db()
+        c = app.app.test_client()
+        login_user(user1, session)
+
+        c.set_cookie("localhost", "token", "asdljnsadlksad")
+        res = c.post("/createtag", json={"tag_name": "tag3"})
+        self.assertEqual(res.status_code, 401)
+
+        c.set_cookie("localhost", "token", user1.token)
+        res = c.post("/createtag")
+        self.assertEqual(res.status_code, 400)
+
+        c.set_cookie("localhost", "token", user1.token)
+        res = c.post("/createtag", json={"tag_name": "tag2"})
+        self.assertEqual(res.status_code, 409)
+
+        c.set_cookie("localhost", "token", user1.token)
+        res = c.post("/createtag", json={"tag_name": "tag3"})
+        self.assertEqual(res.status_code, 200)
+
+
+"""
+    /createcustompage
+    /custompage
+"""
+
+
+class CustomPageEndpointsTestCase(unittest.TestCase):
+    def test_createcustompage_endpoint(self):
+        session, user1, _ = create_db()
+        c = app.app.test_client()
+
         login_user(user1, session)
 
         res = c.post("/createcustompage",
@@ -400,9 +420,6 @@ class CreateCustomPageEndpointTestCase(unittest.TestCase):
                      json={"name": "test", "text": "text", "endpoint": "/testendpoint"})
         self.assertEqual(res.status_code, 200)
 
-
-
-class CustomPageEndpointTestCase(unittest.TestCase):
     def test_custompage_endpoint(self):
         create_db()
         c = app.app.test_client()
